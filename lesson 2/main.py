c4f9c9c6ffe210737bd5e9a93e36c9fd2ab3f060
movavi_school <zoyaterina@mail.ru>
2022-09-29 12:04:31 +0700

    прошёл вторрой урок

---
+++ lesson 2/main.py
@@ -0,0 +1,13 @@
+# sec = int(input("sec: "))
+#
+# h = sec // 3600
+# m = sec % 3600 // 60
+# s = a % 3600 % 60 % 60
+#
+# if h < 10:
+#    h = "0" + str(h)
+# if m < 10
+#    m = "0" + str(m)
+# if s < 10
+#    s = "0" + str(s)
+# print(f"{h}:{m}:{s}" )
