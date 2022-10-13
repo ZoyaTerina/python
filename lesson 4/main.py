faac4620edb3a03e446366f87d266d96b86f2747
movavi_school <Makleplin@gmail.com>
2022-10-13 11:33:47 +0700

    13 октября

---
+++ lesson 4/main.py
@@ -0,0 +1,226 @@
+#familya = input("Фамилия:").capitalize()
+#imya = input("Имя: ").capitalize()
+#otchestvo = input("Отчество:").capitalize()
+
+#print(familya,imya,[0] + ".",otchestvo[0] + ".")
+#print(f"{familya} {imya[0]}.{otchestvo[0]}.")
+
+#word = input("введи вфразу/слово:")
+#print("a:", word.count ("а"))
+
+#phrase = input("введите фразу, разделенную пробелами:").strip()
+            #.strip - удаляет пробелы
+#lst = phrase.split(" ")
+#print(lst)
+#lst.pop(0)  # удалить по индексу
+#lst.remove("антон")  # уадлить по значению
+#print(lst)
+
+#phrase = input("введи фразу:")
+#replace = input("на что меняем:")
+
+#print(phrase.replace(find, replace))
+    # .replace() - замена первого елемента на второй
+
+
+#x = input()
+#print(x.replace("ё", "е"))
+#print(x.replace("йо", "ё"))
+
+#alphabet = {
+ #   "а": "a",
+ #   "б": "b",
+#    "в": "v",
+  #  "г": "g",
+#    "е": "e",
+  #  "ё": "yo",
+#    "ж": "zh",
+ #   "и": "i",
+#    "й": "y",
+#    "к": "k",
+#    "л": "l",
+#    "м": "m",
+#    "н": "n",
+#}
+
+#phrase = input("введите фразу:")
+#translate = ""
+#for bukva in phrase:
+#    translate = translate + alphabet[bukva]
+#print(translate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
