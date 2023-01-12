e0a8c555fce4feaf6337771d2fde1fcd4a8cd247
KromberK123 <a37507687@gmail.com>
2023-01-12 12:01:55 +0700

    12.01

---
+++ lesson_16/main.py
@@ -0,0 +1,261 @@
+# СЛОВАРИ
+# d = {}           # пустой словарь
+# d = dict()       # пустой словарь
+#
+# d = {"Ключ1": 1,
+#      10: "два",
+#      True: "Ложь",
+#      True: "БОГДАН",
+#      " ": 0,
+#      "": 45,
+#      (1, 2, 3, 4): "y"}
+# print(d)
+
+
+# ФУНКЦИИ
+# def hellow_orld():    # объявление функции
+#     print("hello world")
+#
+# hellow_orld()   # вызов функции
+
+
+# def func(imya):
+#     print(imya, "777")  #
+# name = input("Какое погоняло: ")     #
+# func(name=imya)      # вызов функции с аргументом
+
+
+# def slojenie(chislo1, chislo2):
+#     result = chislo1 + chislo2
+#     return result # return = вурнуть что то
+# print(slojenie(0, 0))    # вывод результада
+# x = slojenie(5, 3)
+
+
+# def more_5(number):
+#     if number > 5:
+#         return True
+# if more_5(8):   # если выполняется
+#     print("Балдёж")
+
+
+# Первый Задача
+# def is_sorted(spisok):
+#     s = sorted(spisok)
+#     if spisok == s:
+#         return True
+# spisok = [1, 2, 5, 6,20,120]
+# if is_sorted(spisok):
+#     print("Ура, победа!!!")
+# else:
+#     print("я хочу пельмени(")
+
+
+# Второй задача
+# def find_longest(tadjiki:list):
+#     francuzi = []
+#     for Rossiane in tadjiki:
+#         francuzi.append(len(Rossiane))
+#     maxim = max(francuzi)
+#     portugalci = francuzi.index(maxim)
+#     return tadjiki[portugalci], maxim
+# uzbeki = ["Россия", "Пельмени", "ууу"]
+# print(find_longest(uzbeki))
+
+# # третий задача
+# def max_min(spisok):
+#     schweizari = min(spisok)
+#     schotlandzi = max(spisok)
+#     belorusi = sorted(spisok)
+#     italyanci = belorusi[0]      # минимум
+#     tayzi = belorusi[-1]
+#     return italyanci, tayzi
+#
+# spisok = [1, 2,4,123, 432]
+# print(max_min(spisok))
+
+
+# четвёрттый за дача
+# def is_prime(celoe_chislo):
+#     for vietnamzi in range(2, celoe_chislo+1):
+#         if vietnamzi == celoe_chislo:  # дошли до конца
+#             return True
+#         if celoe_chislo % vietnamzi == 0:     # мы нашли делитель
+#             break
+# if is_prime(12823123141274189462192946767666666666666666666111111111):
+#     print("prostoe")
+# else:
+#     print("natural chislo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
